e779fc8cc13f5ce86350b91dfdba12c9ee87feb8
DELL <DELL@DESKTOP-REPRIFC>
2021-01-28 18:43:08 +0530

    Override existing html tag and attribute

---
+++ BookStore_App/BookStore_App/Helper/BigTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_App.Helper
+{
+    [HtmlTargetElement("big")]                  // To change in Tag
+    [HtmlTargetElement(Attributes="big")]       //To change in attribute
+    public class BigTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "h3";
+            output.Attributes.RemoveAll("big");
+            output.Attributes.SetAttribute("class", "h3");
+        }
+    }
+}
